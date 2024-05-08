e55de155add45fc0d5c0c7ea2ca1ff8f2108f44f
Deea <116511056+deeaion@users.noreply.github.com>
2024-05-08 13:36:16 +0300

    putting all

---
+++ src/main/java/bpos/centerclient/BPositiveCenterClientApplication.java
@@ -0,0 +1,13 @@
+package bpos.centerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BPositiveCenterClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BPositiveCenterClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bpos/centerclient/BPositiveCenterClientApplicationTests.java
@@ -0,0 +1,13 @@
+package bpos.centerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BPositiveCenterClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
